9de6e36abf68d0d233e31a2f088817fd4831b078
Lukasz <lukasz.pietrzyk1991@gmail.com>
2020-08-22 12:02:36 +0200

    add firebase add bootstrap/ng-bootstrap add fontawesome configure firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment.prod';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAYExpaSRqnD12LmmBAmtusgnCQUx1XdGs",
+    authDomain: "strona-ze-smiesznymi-obrazkami.firebaseapp.com",
+    databaseURL: "https://strona-ze-smiesznymi-obrazkami.firebaseio.com",
+    projectId: "strona-ze-smiesznymi-obrazkami",
+    storageBucket: "strona-ze-smiesznymi-obrazkami.appspot.com",
+    messagingSenderId: "812033184169",
+    appId: "1:812033184169:web:18e9d0d3ea838cff0774b8",
+    measurementId: "G-DD2Q7PPW5R"
+  }
 };

66f03c33f8a873f43445b88ca6dffd309f824557
Lukasz <lukasz.pietrzyk1991@gmail.com>
2020-10-08 11:40:10 +0200

    delete not used cycle hooks from buttons-user.component

--- src/app/module/navigation/component/buttons-user/buttons-user.component.ts
+++ src/app/module/navigation/component/buttons-user/buttons-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from 'src/app/module/authentication/service/auth.service';
 
 @Component({
@@ -6,14 +6,10 @@ import { AuthService } from 'src/app/module/authentication/service/auth.service'
   templateUrl: './buttons-user.component.html',
   styleUrls: ['./buttons-user.component.sass']
 })
-export class ButtonsUserComponent implements OnInit {
+export class ButtonsUserComponent {
   private _userLogged: boolean = false;
 
   constructor(public auth: AuthService) {
     this.auth.authState.subscribe(authState => this._userLogged = authState.isLogged );
    }
-
-  ngOnInit() {
-  }
-
 }

a5bd29ec298f1f3c754478567fdbe30ca8ec2d47
Lukasz <lukasz.pietrzyk1991@gmail.com>
2020-10-09 12:16:45 +0200

    profile component styling

--- src/app/module/profile/component/profile/profile.component.ts
+++ src/app/module/profile/component/profile/profile.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DatabaseService } from 'src/app/module/shared/service/database.service';
 
+import { faCamera, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.sass']
 })
 export class ProfileComponent implements OnInit {
+  private _cameraIcon: IconDefinition = faCamera;
+
   private _userID: string = null;
 
   constructor(private route: ActivatedRoute, private dbs: DatabaseService) { 

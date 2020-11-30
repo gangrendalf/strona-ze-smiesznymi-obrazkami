d90527c4722be4e9ca4ab8b0fab1b021d5dfcd0b
Lukasz <lukasz.pietrzyk1991@gmail.com>
2020-11-30 13:57:04 +0100

    add watched-users.component template

--- src/app/module/profile/component/watched-users/watched-users.component.ts
+++ src/app/module/profile/component/watched-users/watched-users.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { delay, map, switchMap, take } from 'rxjs/operators';
+import { UserDetail } from 'src/app/module/shared/model/user.interface';
+import { DatabaseService } from 'src/app/module/shared/service/database.service';
 
 @Component({
   selector: 'watched-users',
   templateUrl: './watched-users.component.html',
   styleUrls: ['./watched-users.component.sass']
 })
-export class WatchedUsersComponent implements OnInit {
+export class WatchedUsersComponent implements OnDestroy {
+  private _subscription: Subscription;
+  public user: UserDetail;
+  public watchedUsers: UserDetail[] = [];
 
-  constructor() { }
+  constructor(private dbs: DatabaseService, private route: ActivatedRoute) {
+    this._subscription = route.pathFromRoot[1].paramMap
+      .pipe(
+        map(params => params.get('uid')),
+        switchMap(uid => dbs.user.getSingle(uid)))
+      .subscribe(user => {
+        if(!user.watchedUsers)
+          return;
 
-  ngOnInit() {
+        let loadedUsers: UserDetail[] = [];
+
+        user.watchedUsers.forEach(async uid => 
+          await this.getUser(uid).then(watchedUser => loadedUsers.push(watchedUser))
+        )
+        
+        this.watchedUsers = loadedUsers;
+        this.user = user;
+      })
+  };
+
+  ngOnDestroy() {
+    this._subscription.unsubscribe();
   }
 
+  private getUser(uid) {
+    return this.dbs.user.getSingle(uid).pipe(take(1)).toPromise();
+  }
 }

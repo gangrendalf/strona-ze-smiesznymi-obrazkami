33e72fca13e8626395da614a917ee9908ed358ed
Lukasz <lukasz.pietrzyk1991@gmail.com>
2020-12-04 09:29:38 +0100

    code refactoring - auth guards simplify

--- src/app/module/authentication/guard/auth.guard.ts
+++ src/app/module/authentication/guard/auth.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../service/auth.service';
 import { take, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,8 @@ export class AuthGuard implements CanActivate {
   
   constructor(private auth: AuthService) { }
 
-  async canActivate(): Promise<boolean> {
-    
-    return (await this.auth.authState.pipe(take(1)).toPromise()).isLogged;
+  canActivate(): Observable<boolean> {
+    return this.auth.authState.pipe(map(authUser => authUser.isLogged));
   }
   
 }
--- src/app/module/authentication/guard/un-auth.guard.ts
+++ src/app/module/authentication/guard/un-auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { take, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { AuthService } from '../service/auth.service';
 
 @Injectable({
@@ -10,8 +10,8 @@ import { AuthService } from '../service/auth.service';
 export class UnAuthGuard implements CanActivate {
   constructor(private auth: AuthService) { }
 
-  async canActivate(): Promise<boolean> {
-    return !(await this.auth.authState.pipe(take(1)).toPromise()).isLogged
+  canActivate(): Observable<boolean> {
+    return this.auth.authState.pipe(map(authUser => !authUser.isLogged));
   }
   
 }
